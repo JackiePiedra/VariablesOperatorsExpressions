36f105c37f94df266443faae26bdab63a84a9d6a
JackiePiedra <jackie.r.piedra@gmail.com>
2017-08-16 20:51:48 -0400

    Variable work

--- VariablesOperatorsExpressions/Program.cs
+++ VariablesOperatorsExpressions/Program.cs
@@ -10,121 +10,125 @@ namespace VariablesOperatorsExpressions
     {
         static void Main(string[] args)
         {
-            string name = "Jackie";
-            //Console.WriteLine(name);
+            /* string name = "Jackie";
+             //Console.WriteLine(name);
 
-            name = "Dave";
-            //Console.WriteLine(name);
+             name = "Dave";
+             //Console.WriteLine(name);
 
-            string birthMonth = "September";
-            //Console.WriteLine(birthMonth);
-            //integers
-            int birthMonthNumber = 9;
-            int age = 33;
-            int population = 100000;
-            //Floating type variables
-            float number = 2.164403304897294383961032f;
-            double someNumber = 2.164403304897294383961032d;
-            decimal partialNumber = 2.164403304897294383961032m;
+             string birthMonth = "September";
+             //Console.WriteLine(birthMonth);
+             //integers
+             int birthMonthNumber = 9;
+             int age = 33;
+             int population = 100000;
+             //Floating type variables
+             float number = 2.164403304897294383961032f;
+             double someNumber = 2.164403304897294383961032d;
+             decimal partialNumber = 2.164403304897294383961032m;
 
-            //Console.WriteLine(number);
-            //Console.WriteLine(someNumber);
-            //Console.WriteLine(partialNumber);
+             //Console.WriteLine(number);
+             //Console.WriteLine(someNumber);
+             //Console.WriteLine(partialNumber);
 
-            //Boolean
-            bool isPresent = true;
-            bool seatTaken = false;
+             //Boolean
+             bool isPresent = true;
+             bool seatTaken = false;
 
-            //Character
-            char lastLetter = 'p';
+             //Character
+             char lastLetter = 'p';
 
-            int jessicaAge = 23;
-            int samAge = 47;
-            Console.WriteLine(jessicaAge > samAge);
+             int jessicaAge = 23;
+             int samAge = 47;
+             Console.WriteLine(jessicaAge > samAge);
 
-            //Mike and three friends split a bill
-            //Each person pays $13
-            //What is the total bill?
-            int numberGuests = 4;
-            int guestPay = 13;
-            Console.WriteLine(numberGuests * guestPay);
+             //Mike and three friends split a bill
+             //Each person pays $13
+             //What is the total bill?
+             int numberGuests = 4;
+             int guestPay = 13;
+             Console.WriteLine(numberGuests * guestPay);
 
-            //Lisa needs 7 cups sugar
-            //She's used 2 cups
-            //How many cups does she need?
-            int totalNeeded = 7;
-            int totalUsed = 2;
-            Console.WriteLine(totalNeeded - totalUsed);
+             //Lisa needs 7 cups sugar
+             //She's used 2 cups
+             //How many cups does she need?
+             int totalNeeded = 7;
+             int totalUsed = 2;
+             Console.WriteLine(totalNeeded - totalUsed);
 
-            //How many packages of diapers can you buy for $40 if each costs $8
-            int totalMoney = 40;
-            int diaperPrice = 8;
-            Console.WriteLine(totalMoney / diaperPrice);
+             //How many packages of diapers can you buy for $40 if each costs $8
+             int totalMoney = 40;
+             int diaperPrice = 8;
+             Console.WriteLine(totalMoney / diaperPrice);
 
-            int fridayMoney = 29;
-            int moneyNow = 41;
-            Console.WriteLine(moneyNow - fridayMoney);
+             int fridayMoney = 29;
+             int moneyNow = 41;
+             Console.WriteLine(moneyNow - fridayMoney);
 
-            int juliaMiles = 47;
-            //int pranavMiles = juliaMiles - 30;
-            //Console.WriteLine(pranavMiles);
-            //Console.WriteLine(juliaMiles - 30);
-            int milesMore = 30;
-            int pranavMiles = juliaMiles - milesMore;
-            Console.WriteLine(pranavMiles);
+             int juliaMiles = 47;
+             //int pranavMiles = juliaMiles - 30;
+             //Console.WriteLine(pranavMiles);
+             //Console.WriteLine(juliaMiles - 30);
+             int milesMore = 30;
+             int pranavMiles = juliaMiles - milesMore;
+             Console.WriteLine(pranavMiles);
 
-            int boxPrice = 3;
-            int moneyAvailable = 12;
-            //Console.WriteLine(moneyAvailable / boxPrice);
-            int envelopes = moneyAvailable / boxPrice;
-            Console.WriteLine(envelopes);
+             int boxPrice = 3;
+             int moneyAvailable = 12;
+             //Console.WriteLine(moneyAvailable / boxPrice);
+             int envelopes = moneyAvailable / boxPrice;
+             Console.WriteLine(envelopes);
 
-            float saladPrice = 5.12f;
-            float endBalance = 27.10f;
-            //Console.WriteLine(saladPrice + endBalance);
-            float begBalance = saladPrice + endBalance;
-            Console.WriteLine(begBalance);
+             float saladPrice = 5.12f;
+             float endBalance = 27.10f;
+             //Console.WriteLine(saladPrice + endBalance);
+             float begBalance = saladPrice + endBalance;
+             Console.WriteLine(begBalance);
 
-            //331 students
-            //6 buses and 7 students traveled in cars
-            int total = 331;
-            //int perBus = (total-7)/6;
-            int buses = 6;
-            int driven = 7;
-            int perBus = (total - driven) / buses;
-            Console.WriteLine(perBus);
+             //331 students
+             //6 buses and 7 students traveled in cars
+             int total = 331;
+             //int perBus = (total-7)/6;
+             int buses = 6;
+             int driven = 7;
+             int perBus = (total - driven) / buses;
+             Console.WriteLine(perBus);
 
-            float maxMoney = 24.0f;
-            int pencilsBought = 7;
-            float leftoverMoney = 10.0f;
-            Console.WriteLine((maxMoney - leftoverMoney) / pencilsBought);
-            //Console.WriteLine(perPencil);
+             float maxMoney = 24.0f;
+             int pencilsBought = 7;
+             float leftoverMoney = 10.0f;
+             Console.WriteLine((maxMoney - leftoverMoney) / pencilsBought);
+             //Console.WriteLine(perPencil);
 
-            //3 consecutive numbers equal 72
-            //int sum = 72;
-            //int smallestX =(sum - 3) / 3;
-            //Console.WriteLine(smallestX);
-            int totalNumber = 72;
-            int midNumber = totalNumber / 3;
-            int maxNumber = midNumber + 1;
-            int minNumber = midNumber - 1;
-            Console.WriteLine(minNumber);
+             //3 consecutive numbers equal 72
+             //int sum = 72;
+             //int smallestX =(sum - 3) / 3;
+             //Console.WriteLine(smallestX);
+             int totalNumber = 72;
+             int midNumber = totalNumber / 3;
+             int maxNumber = midNumber + 1;
+             int minNumber = midNumber - 1;
+             Console.WriteLine(minNumber);
 
-            //3 consecutive even numbers total 48
-            //int sum = 48;
-            //int smallestX =(sum - 6) / 3;
-            //Console.WriteLine(smallestX);
-            int totalSum = 48;
-            int middleNumber = totalSum / 3;
-            int bigNumber = middleNumber + 2;
-            int smallNumber = middleNumber - 2;
-            Console.WriteLine(smallNumber);
+             //3 consecutive even numbers total 48
+             //int sum = 48;
+             //int smallestX =(sum - 6) / 3;
+             //Console.WriteLine(smallestX);
+             int totalSum = 48;
+             int middleNumber = totalSum / 3;
+             int bigNumber = middleNumber + 2;
+             int smallNumber = middleNumber - 2;
+             Console.WriteLine(smallNumber);
 
-            int endBoxes = 22;
-            int boxesBought = 7;
-            int begBoxes = (endBoxes * 2) - 7;
-            Console.WriteLine(begBoxes);
+             int endBoxes = 22;
+             int boxesBought = 7;
+             int begBoxes = (endBoxes * 2) - 7;
+             Console.WriteLine(begBoxes);
+             */
 
+            Console.WriteLine("Please enter your age");
+            int userAge = int.Parse(Console.ReadLine());
+            //Console.WriteLine(userAge + 10);
         }
     }
 }
